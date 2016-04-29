b1fb1361d986f0571d9e2a825d75e0dc8b097842
Gonzalo Barco <gonzalo.barco@globant.com>
2016-04-29 03:13:28 -0300

    Working TT route.

--- wsTemplate/lib/wsTemplate/Helper/TT.pm
+++ wsTemplate/lib/wsTemplate/Helper/TT.pm
@@ -1,9 +1,33 @@
 package wsTemplate::Helper::TT;
 
+use strict;
+use warnings;
 use Moose;
 with 'wsTemplate::Helper';
 
-sub mergeTemplateUnicornWithParametersPanda {return 1;};
+use Template;
+use JSON qw();
+use Try::Tiny;
+
+use wsTemplate::Exception;
+
+sub mergeTemplateUnicornWithParametersPanda {
+	my ($self, $template, $parameters) = @_;
+	my $output;
+
+	my $tt = Template->new();
+	my $vars;
+
+	try {
+		$vars = JSON::from_json($parameters->content);
+	} catch {
+		wsTemplate::Exception::BadJSON->throw(error=>$_);
+	};
+		
+	$tt->process($template->file_handle,$vars,\$output);
+
+	return $output;
+};
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
