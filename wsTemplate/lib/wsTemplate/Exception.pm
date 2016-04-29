21ebec0fba15e5b4b4d325c9b9b948ce2e338c5c
Gonzalo Barco <gonzalo.barco@globant.com>
2016-04-29 03:11:52 -0300

    Add BadJSON Exception.

--- wsTemplate/lib/wsTemplate/Exception.pm
+++ wsTemplate/lib/wsTemplate/Exception.pm
@@ -3,6 +3,7 @@ package wsTemplate::Exception;
 use Exception::Base
 	'wsTemplate::Exception::Error' => {has => ['error']},
 	'wsTemplate::Exception::FailedToInstanceHelperClass' => { isa => 'wsTemplate::Exception::Error' },
-	'wsTemplate::Exception::MuggleHelper' => { isa => 'wsTemplate::Exception::Error' };
+	'wsTemplate::Exception::MuggleHelper' => { isa => 'wsTemplate::Exception::Error' },
+	'wsTemplate::Exception::BadJSON' => { isa => 'wsTemplate::Exception::Error' };
 
 1;
