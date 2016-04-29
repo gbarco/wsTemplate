8d08eae4e2afe107dbf956bba82d314fa5416c37
Gonzalo Barco <gonzalo.barco@globant.com>
2016-04-28 22:52:03 -0300

    Non working webservice routes sketch.

---
+++ wsTemplate/lib/wsTemplate.pm
@@ -0,0 +1,63 @@
+package wsTemplate;
+use strict;
+use warnings;
+
+use Dancer ':syntax';
+
+use Try::Tiny;
+use JSON;
+
+require wsTemplate::ErrorHandler;
+
+our $VERSION = '0.1';
+
+get '/' => sub {
+	template 'index';
+};
+
+get '/version' => sub {
+	content_type 'application/json';
+	
+	return to_json { version => $VERSION };
+};
+
+post '/ws/:templateType/' => sub {
+
+	my $helperInstantializationFailed = 0;
+	my $userContent = 0;
+	my $helperClassName;
+
+	params->{templateType} =~ /^((tpl)|(tt))$/;
+	$helperClassName = 'wsTemplate::' . $1;
+
+	use wsTemplate::HelperFactory;
+
+	wsTemplate::HelperFactory->canInstance();	
+
+	my $helperClass;
+	my $userReadableResponse;	
+
+	try {
+		$helperClass = wsTemplate::HelperFactory->instance(params->{templateType});
+	} catch {
+		my $exception = $_;
+		$userReadableResponse = wsTemplate::ErrorHandler->getUserReadableResponseForException($exception);
+	}
+
+	unless ($helperClass) {
+		my $templateUnicorn = request->upload('template');
+		my $parametersPanda = request->upload('vars');
+
+		try {
+			my $templatePandicorn = $helperClass->mergeUnicornWithPanda($templateUnicorn, $parametersPanda);
+			$userRedableResponse = $templatePandicorn || wsTemplate::ErrorHandler->getUserReadableResponseForException(new Exception::HelperProducedNoOutput);
+		} catch {
+			my $exception = $_;
+			$userReadableResponse = wsTemplate::ErrorHandler->getUserReadableResponseForException($exception);
+		}
+	}
+
+	return $userReadableResponse;
+};
+
+true;
