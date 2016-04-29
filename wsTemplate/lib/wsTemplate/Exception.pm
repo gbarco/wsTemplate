7c7bbe6a133d29d908e9168e936e9f240417c9bb
Gonzalo Barco <gonzalo.barco@globant.com>
2016-04-29 00:27:45 -0300

    Rename ::Exception::MuggleClass to ::MuggleHelper

--- wsTemplate/lib/wsTemplate/Exception.pm
+++ wsTemplate/lib/wsTemplate/Exception.pm
@@ -3,6 +3,6 @@ package wsTemplate::Exception;
 use Exception::Base
 	'wsTemplate::Exception::Error' => {has => ['error']},
 	'wsTemplate::Exception::FailedToInstanceHelperClass' => { isa => 'wsTemplate::Exception::Error' },
-	'wsTemplate::Exception::MuggleClass' => { isa => 'wsTemplate::Exception::Error' };
+	'wsTemplate::Exception::MuggleHelper' => { isa => 'wsTemplate::Exception::Error' };
 
 1;
