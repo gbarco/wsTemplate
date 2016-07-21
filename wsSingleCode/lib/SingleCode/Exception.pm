edfdeec815e0ea0772e44642ae03fe2db97e5a7d
Gonzalo Barco <gonzalo.barco@globant.com>
2016-07-21 12:33:43 -0400

    Update FlightRange exceptions names.

--- wsSingleCode/lib/SingleCode/Exception.pm
+++ wsSingleCode/lib/SingleCode/Exception.pm
@@ -2,8 +2,8 @@ package SingleCode::Exception;
 
 use Exception::Base
   'SingleCode::Exception::Error' => { has => ['error'] },
-  'SingleCode::Exception::AirlineCodeUnknown' => { isa => 'SingleCode::Exception::Error' },
-  'SingleCode::Exception::FlightNumberFormat' => { isa => 'SingleCode::Exception::Error' },
-  'SingleCode::Exception::FlightDateFormat' => { isa => 'SingleCode::Exception::Error' },
+  'SingleCode::Exception::FlightRangeDatesInvalid' => { isa => 'SingleCode::Exception::Error' },
+  'SingleCode::Exception::FlightDateInvalid' => { isa => 'SingleCode::Exception::Error' },
+  'SingleCode::Exception::FlightDesignatorFormat' => { isa => 'SingleCode::Exception::Error' },
 
 1;
