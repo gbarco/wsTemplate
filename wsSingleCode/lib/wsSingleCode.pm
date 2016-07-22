8789760490408718aa2d38b90d82f65091ff6b96
Gonzalo Barco <gonzalo.barco@globant.com>
2016-07-22 15:05:19 -0400

    Deleted data set endpoints. Fixed ErrorHandler namespace.

--- wsSingleCode/lib/wsSingleCode.pm
+++ wsSingleCode/lib/wsSingleCode.pm
@@ -3,11 +3,10 @@ use strict;
 use warnings;
 
 use Dancer ':syntax';
-set serializer => mutable;
 
 use Try::Tiny;
 
-require SingleCode::ErrorHandler;
+require wsSingleCode::ErrorHandler;
 
 our $VERSION = '0.1';
 
@@ -22,38 +21,6 @@ get '/version' => sub {
 get '/livelyness' => sub {
 	return { live => 1 };
 };
-
-post '/ws/setFlightData/:flightDate' => sub {
-	require SingleCode::FlightData;
-
-	my $serviceReadableResponse;
-	my $flightData = SingleCode::FlightData->new();
-	my $flightDate = params->{flightDate};
-
-	try {
-		$serviceReadableRespose = $flightData->setFlightData(request->upload('flightData'),$flightDate);
-	} catch {
-		$serviceReadableResponse = wsSingleCode::ErrorHandler::getResponseFromException($_);
-	}
-
-	return $serviceReadableResponse;
-}
-
-get '/ws/getFlightData/:flightDate' => sub {
-	require SingleCode::FlightData;
-
-	my $serviceReadableResponse;
-	my $flightData = SingleCode::FlightData->new();
-	my $flightDate = params->{flightDate};
-
-	try {
-		$serviceReadableRespose = $flightData->getFlightData($flightDate);
-	} catch {
-		$serviceReadableResponse = wsSingleCode::ErrorHandler::getResponseFromException($_);
-	}
-
-	return $serviceReadableResponse;
-}
 
 get '/ws/getFlightRangeInformation/:flightDesignator/:flightDate' => sub {
 	require SingleCode::FlightInformation;
